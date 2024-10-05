8c7f00e826dac133758472e71bfe02f69465e39b
Yahia Elghoniemy <yahiaamr90@gmail.com>
2024-10-05 18:13:23 +0300

    tourist can register

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ app.use("/api/activity", activityRoutes);
 app.use("/api/product",productRoutes);
 app.use("/api/itinerary",itineraryRoutes);
 app.use("/api/tourGuide",tourGuide);
+app.use("/api/tourist",touristRoutes);
 app.use("/api/prefrenceTag",preferencetagRoute);
 app.listen(PORT, () => {
     connectDB();
