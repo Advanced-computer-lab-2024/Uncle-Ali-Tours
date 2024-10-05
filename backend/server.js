0eb9a271ab7a29733960600ea5adfa3a02e1f44a
RabieZewil <rabiezewil@gmail.com>
2024-10-05 15:30:11 +0300

    merging two puch

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import activityCategoryRouts from './routes/activityCategory.route.js';
 import itineraryRoutes from './routes/itinerary.route.js';
 import productRoutes from './routes/product.routes.js';
 import touristRoutes from './routes/tourist.route.js';
+import userRouts from './routes/user.route.js';
+import tourGuide from './routes/tourGuide.route.js'
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +17,8 @@ app.use("/api/activityCategory", activityCategoryRouts);
 app.use("/api/activity", activityRoutes);
 app.use("/api/product",productRoutes);
 app.use("/api/tourist",touristRoutes);
+app.use("/api/itinerary",itineraryRoutes);
+app.use("/api/tourGuide",tourGuide);
 app.listen(PORT, () => {
     connectDB();
     console.log(`server is running on port ${PORT}`);
