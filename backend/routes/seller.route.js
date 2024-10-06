8a87f7735e25f62fb08318a479a495ba0d7b26cd
YoussefMansoor <135554908+YoussefMansoor@users.noreply.github.com>
2024-10-06 19:44:49 +0300

    sellerprofile

--- backend/routes/seller.route.js
+++ backend/routes/seller.route.js
@@ -2,9 +2,9 @@ import express from 'express';
 import { createSeller, getSellerByName, updateSeller, deleteSeller } from '../controllers/SellerController.js';
 
 const router = express.Router();
-router.post('/sellers', createSeller);
-router.get('/sellers/:name', getSellerByName);
-router.put('/sellers/:name', updateSeller);
-router.delete('/sellers/:name', deleteSeller);
+router.post('/', createSeller);
+router.get('/', getSellerByName);
+router.put('/', updateSeller);
+router.delete('/', deleteSeller);
 
 export default router;
