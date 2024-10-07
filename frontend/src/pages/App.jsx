7b559f77411fbeb74ec305f06bfd9b465a54ddd2
YoussefMansoor <135554908+YoussefMansoor@users.noreply.github.com>
2024-10-07 23:43:37 +0300

    crudactivity

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -19,6 +19,7 @@ import AdminDashboard from "./AdminDashboardPage";
 import TourGuideProfilePage from "./TourGuideProfilePage";
 import Productpage from "./Productpage";
 import TouristProfile from './TouristProfile';
+import ActivitiesPage from "./crudActivities";
 function App() {
   return (
     <div className="rounded-lg shadow-lg text-center text-[#1e1e2e] min-h-[calc(100vh-2vh)] mt-[1vh] w-[calc(100vw-2vh)] ml-[1vh] border-2 border-[#23263400] backdrop-blur-xl text-white font-black bg-[#161821f0] ">
@@ -41,10 +42,9 @@ function App() {
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/product" element={<Productpage />} />
         <Route path="/profile" element={<TouristProfile />} />
-
         <Route path="/updateItinerary" element={<UpdateItinerary />} />
         <Route path="/TourGuideProfilePage" element={<TourGuideProfilePage/>}/>
-        
+        <Route path="/crudActivity" element={<ActivitiesPage/>}/>
       </Routes>
     </div>
   );
