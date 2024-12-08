c451411a9e8a2094981deea1e6b5fac34e1981bc
AliFadll <161974130+AliFadll@users.noreply.github.com>
2024-12-08 16:05:47 +0200

    apply promo code in payment

--- backend/routes/promo.route.js
+++ backend/routes/promo.route.js
@@ -2,9 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
-import { create, list } from '../controllers/promo.controller.js';
+import { applyPromoCode, create, list } from '../controllers/promo.controller.js';
 
 router.post('/', create);
 router.get('/', list);
+router.post('/applyPromo',applyPromoCode);
 
 export default router;
